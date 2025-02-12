87282a22a9e7817274ee3bebbdd99441f74f939d
Al Mehedi <leftoverm9@gmail.com>
2025-02-12 16:54:52 +0600

    pre testing

--- src/core/parser.zig
+++ src/core/parser.zig
@@ -1,14 +1,24 @@
 const std = @import("std");
 const json = std.json;
+const builtin = std.builtin;
 const Allocator = std.mem.Allocator;
+const StructField = builtin.Type.StructField;
+
+
+const Error = std.mem.Allocator.Error;
 
 
 pub const Static = struct {
     pub fn parse(comptime T: type, heap: Allocator, data: []const u8) !T {
         const parsed = try json.parseFromSlice(T, heap, data, .{});
         defer parsed.deinit();
-        return parsed.value;
+
+        return try deepCopy(heap, parsed.value);
     }
+
+    // pub fn free(heap: Allocator, result: anytype) !void {
+    //     try deepFree(heap, result);
+    // }
 
     /// **WARNING:** Return value must be deallocated!
     pub fn stringify(heap: Allocator, value: anytype) ![]u8 {
@@ -19,6 +29,95 @@ pub const Static = struct {
         return out;
     }
 };
+
+// fn deepFree(heap: Allocator, src: anytype) !void {
+//     switch (@typeInfo(@TypeOf(src))) {
+//         .@"struct" => |s| {
+//             inline for (s.fields) |field| {
+//                 try freeFieldValue(heap, src, field.type, field.name);
+//             }
+//         },
+//         else => @compileError("Unsupported Type")
+//     }
+// }
+
+// fn freeFieldValue(
+//     heap: Allocator,
+//     src: anytype,
+//     comptime T: type,
+//     comptime tag: []const u8
+// ) !void {
+//     const value = @field(src, tag);
+
+//     switch (@typeInfo(T)) {
+//         .@"struct" => |_| {
+//             deepFree(heap, value);
+//         },
+//         .pointer => |_| {
+//             heap.free(value);
+//         },
+//         .optional => |o| {
+//             if (value != null) freeFieldValue(heap, src, o.child, tag);
+//         },
+//         else => {} // NOP
+//     }
+// }
+
+fn deepCopy(heap: Allocator, src: anytype) Error!@TypeOf(src) {
+    var dest: @TypeOf(src) = undefined;
+
+    switch (@typeInfo(@TypeOf(src))) {
+        .@"struct" => |s| {
+            inline for (s.fields) |field| {
+                const value = @field(src, field.name);
+                const v = try copyFieldValue(heap, @TypeOf(value), value);
+                @field(dest, field.name) = v;
+            }
+        },
+        .pointer => |p| {
+            if (p.is_const and p.size == .slice) {
+                const slice = try heap.alloc(p.child, src.len);
+
+                var i: usize = 0;
+                while (i < src.len) : (i += 1) {
+                    const value = src[i];
+                    slice[i] = try copyFieldValue(heap, @TypeOf(value), value);
+                }
+
+                dest = slice;
+            } else @compileError("Only Use `[]const T` Instead");
+        },
+        else => @compileError("Unsupported Type")
+    }
+
+    return dest;
+}
+
+fn copyFieldValue(heap: Allocator, comptime T: type, value: T) Error!T {
+    switch (@typeInfo(T)) {
+        .bool => return value,
+        .int => |n| {
+            if (n.signedness == .unsigned and n.bits == 8) return value 
+            else if (n.signedness == .signed and n.bits == 64) return value
+            else @compileError("Only Use `u8` or `i64` Instead");
+        },
+        .float => |f| {
+            if (f.bits == 64) return value
+            else @compileError("Only Use `f64` Instead");
+        },
+        .@"struct" => return try deepCopy(heap, value),
+        .pointer => return try deepCopy(heap, value),
+        .optional => |o| {
+            if (value == null) return value
+            else return try copyFieldValue(heap, o.child, value.?);
+        },
+        else => @compileError("Unsupported Field Type")
+    }
+}
+
+
+
+
 
 pub const Dynamic = struct {
     parsed: json.Parsed(json.Value),
--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,28 @@
 const std = @import("std");
+const mem = std.mem;
 const debug = std.debug;
+const Allocator = mem.Allocator;
 
 const parser = @import("./core/parser.zig");
 
 
 pub fn main() !void {
     var gpa_mem = std.heap.GeneralPurposeAllocator(.{}){};
-    defer debug.assert(gpa_mem.deinit() == .ok);
+    // defer debug.assert(gpa_mem.deinit() == .ok);
     const heap = gpa_mem.allocator();
 
     // Static JSON
-    const static_input = "{ \"name\": \"John Doe\", \"age\": 40 }";
-    const User = struct { name: []const u8, age: u8 };
+    const out = try staticJsonTest(heap);
+    debug.print("Result: {any}\n", .{out});
 
-    const data = try parser.Static.parse(User, heap, static_input);
-    debug.print(
-        "Static Data [ name: {s}, age: {d} ]\n", .{data.name, data.age}
-    );
+    const str = try parser.Static.stringify(heap, out);
+    defer heap.free(str);
+    debug.print("{s}\n", .{str});
+    // try parser.Static.free(heap, out);
 
-    const out = try parser.Static.stringify(heap, data);
-    defer heap.free(out);
 
-    debug.print("{s}\n", .{out});
+
+
 
     // Dynamic JSON with object data
     const input2 =
@@ -55,4 +56,45 @@ pub fn main() !void {
     const json_data2 = dyn_json2.data().array;
     const item = json_data2.items[0].string;
     std.debug.print("Array Item: {s}\n", .{item});
+}
+
+const User = struct {
+    name: []const u8,
+    age: i64,
+    balance: f64,
+    score: []const i64,
+    hobby: []const[]const u8,
+    card_one: Card,
+    card_two: ?Card,
+    card_three: ?Card,
+    wallet: []const ?Card
+};
+
+const Card = struct { name: []const u8, credit: i64 };
+
+fn staticJsonTest(heap: Allocator) !User {
+    const static_str =
+    \\ {
+    \\      "name": "John Doe",
+    \\      "age": 40,
+    \\      "balance": 5000.78,
+    \\      "score": [ 120, 240, 301 ],
+    \\      "hobby": [ "fishing", "reading" ],
+    \\      "card_one": { "name": "Green", "credit": 500 },
+    \\      "card_two": { "name": "yellow", "credit": 10 },
+    \\      "card_three": null,
+    \\      "wallet": [
+    \\          { "name": "Green", "credit": 500 },
+    \\          { "name": "yellow", "credit": 10 },
+    \\          null
+    \\      ]
+    \\ }
+    ;
+
+    const static_input = try heap.alloc(u8, static_str.len);
+    mem.copyForwards(u8, static_input, static_str);
+    defer heap.free(static_input);
+
+    const data = try parser.Static.parse(User, heap, static_input);
+    return data;
 }
